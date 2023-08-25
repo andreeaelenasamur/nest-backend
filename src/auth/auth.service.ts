fd0d5ad177a46b0d073bfd23b27a507f915cca10
Andreea <andelenasamur@gmail.com>
2023-08-25 09:32:50 +0200

    user registration and interfaces

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,11 +5,10 @@ import { Model } from 'mongoose';
 import * as bcryptjs from 'bcryptjs';
 import { JwtService } from '@nestjs/jwt';
 
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { UpdateAuthDto, CreateUserDto, LoginDto, RegisterUserDto } from './dto';
 import { User } from './entities/user.entity';
-import { LoginDto } from './dto/login.dto';
 import { JwtPayload } from './interfaces/jwt-payload';
+import { LoginResponse } from './interfaces/login-response';
 
 @Injectable()
 export class AuthService {
@@ -43,7 +42,18 @@ export class AuthService {
     }
   }
 
-  async login( loginDto: LoginDto ) {
+  async register(registerUserDto: RegisterUserDto): Promise<LoginResponse> {
+
+    const user = await this.create(registerUserDto);
+
+    return {
+      user: user,
+      token: this.getJwtToken({ id: user._id })
+    }
+  }
+
+
+  async login( loginDto: LoginDto ):Promise<LoginResponse> {
     const { email, password } = loginDto;
 
     const user = await this.userModel.findOne({ email });
@@ -64,7 +74,6 @@ export class AuthService {
     }
 
   }
-
 
   findAll() {
     return `This action returns all auth`;
---
+++ src/auth/interfaces/register-reponse.ts
@@ -0,0 +1,8 @@
+import { User } from "../entities/user.entity";
+
+export interface RegisterResponse {
+
+    user: User;
+    token: string;
+
+}
